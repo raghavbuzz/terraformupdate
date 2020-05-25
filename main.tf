318e4b094a6a2da63b4cea12636bc2035781023e
rsharma97 <rsharma97@dxc.com>
2020-05-26 01:27:10 +0530

    add main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
     version = "2.5.0"
-    features {}
+    features {}    
 }
 
 resource "azurerm_resource_group" "tf_test" {
@@ -8,3 +8,23 @@ resource "azurerm_resource_group" "tf_test" {
     location = "Central India"
 }
 
+resource "azurerm_container_group" "tfcg_test" {
+    name = "weatherapi"
+    location = azurerm_resource_group.tf_test.location
+    resource_group_name = azurerm_resource_group.tf_test.name
+
+    ip_address_type = "public"
+    dns_name_label = "raghavbuzzwa"
+    os_type = "Linux"
+
+    container {
+        name = "weatherapi"
+        image = "raghavbuz/weatherapi"
+        cpu = "1"
+        memory = "1"
+        ports {
+            port = 80
+            protocol = "TCP"
+        }
+    }
+}
