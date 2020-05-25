51a44d3bfc1e941bab5aa365ff6b9a1021fe4600
rsharma97 <rsharma97@dxc.com>
2020-05-26 02:49:54 +0530

    Fixed our pipeline

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ terraform {
     }
 }
 
+variable "imagebuild" {
+  type        = string  
+  description = "Latest Image Build"
+}
+
+
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location = "Central India"
@@ -28,7 +34,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
     container {
         name = "weatherapi"
-        image = "raghavbuz/weatherapi"
+        image = "raghavbuz/weatherapi:${var.imagebuild}"
         cpu = "1"
         memory = "1"
         ports {
